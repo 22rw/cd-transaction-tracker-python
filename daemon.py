bb75cef4a60b9f7507f00c7177c767cc945b8153
22rw <a_div@tutanota.com>
2023-09-02 00:00:27 +0200

    First working draft

--- daemon.py
+++ daemon.py
@@ -1,27 +1,54 @@
 import configparser
+from datetime import date, datetime, timedelta
 from comdirect_api.comdirect_client import ComdirectClient
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
 
-    with open('creds.ini') as credfile:
-        config.read(credfile)
-        client_id = config['clientId']
-        client_secret = config['clientSecret']
+    config.read('creds.ini')
 
-    username = input('Please input your comdirect username.')
-    pin = input('Please input your pin')
+    if 'creds' not in config:
+        print('Error: please supply your credentials, then restart!')
+
+    client_id = config['creds']['clientId']
+    client_secret = config['creds']['clientSecret']
+
+    username = input('Please input your comdirect username \n')
+    pin = input('Please input your pin \n')
 
     client = ComdirectClient(client_id, client_secret)
 
-    input('Running initial flow, do not forget to activate the TAN in your photoTAN App')
+    input('Running initial flow, do not forget to activate the TAN in your photoTAN App \nPress ENTER to continue...')
 
     client.fetch_tan(username, pin)
 
+    input('Press ENTER after completing the TAN challenge...')
+
     client.activate_session()
 
-    balances = client.get_all_balances()
-    print(balances['values'])
+    transactions = client.get_account_transactions(
+        '07782CC76AF84928A0D1271993A8C55F',
+        False,
+        'BOTH',
+        20,
+        0,
+        str((datetime.today() - timedelta(days=2)).date()),
+        str(date.today())
+    )
+
+    for transaction in transactions['values']: 
+        print(
+            'remitter: '
+            + str(transaction['remitter']) 
+            + ' | debtor: ' 
+            + str(transaction['debtor'])
+            + ' | creditor: '
+            + str(transaction['creditor'])
+            + ' | amount: '
+            + str(transaction['amount'])
+        )
+
+    input('Press ENTER to exit...')
 
     
 
